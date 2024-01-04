1f0d01464bfe6cd23ea39013a4b4f5d4a9b347c6
Alexei Kravchuk <a.kravchuk@freeplay.io>
2024-01-04 22:38:35 +0100

    init project with base logic

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface TodoListProps {
+  items: {
+    id: string,
+    text: string
+  }[]
+}
+
+function TodoList(props: TodoListProps) {
+  return <ul>
+    {props.items.map(todo => <li key={todo.id}>
+      {todo.text}
+    </li>)}
+  </ul>
+}
+
+export default TodoList
